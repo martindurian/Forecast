6838bf4abeeeeccdb478459981ed1e99e6c55f11
Martin Durian <durian.m@daikin.sk>
2021-04-23 09:16:11 +0200

    present used in DCS reporting

--- forecast_functions_for_DCS.R
+++ forecast_functions_for_DCS.R
@@ -14,7 +14,7 @@ prepare_df_fun <- function(Data_struct_fun) map(Data_struct_fun$Out_DF_list_outd
   
   outdoor_inc_tec_df <- outdoor_df %>%
     mutate(OpeMode=if_else(Cooling.capacity==0 & Heating.capacity==0 & Operation.control.mode == "Heat recovery normal control", "HeRec", OpeMode)) %>% 
-    select(TimeStampPosixct, consumption = Watt.hour.amount.Corrected., ambient = Outside.air.temp., OpeMode, Operation.control.mode, UniqueIdentifier, UnitType, SystemIdentifier, ope_time_out = Outdoor.unit.operation.time, matches('^Te.mean.value|^Tc.mean.value|^Tes.mean.value|^Tcs.mean.value')) %>%   #'^T.*\\.mean\\.value' %>% 
+    select(TimeStampPosixct, consumption = Watt.hour.amount.Corrected., ambient = Outside.air.temp., OpeMode, Operation.control.mode, UniqueIdentifier, UnitType, Line_Name, SystemIdentifier, ope_time_out = Outdoor.unit.operation.time, matches('^Te.mean.value|^Tc.mean.value|^Tes.mean.value|^Tcs.mean.value')) %>%   #'^T.*\\.mean\\.value' %>% 
     mutate(consumption = replace(consumption, consumption == 0, 0.015), dummy.worktime_out = if_else(ope_time_out == 0, 0, 1)) %>% 
     arrange(TimeStampPosixct)
   
@@ -25,28 +25,36 @@ prepare_df_fun <- function(Data_struct_fun) map(Data_struct_fun$Out_DF_list_outd
     pluck("Merged_Indoor_Units") %>%
     arrange(TimeStampPosixct) %>% 
     filter(SystemIdentifier %in% sys_ident) %>% 
-    select(TimeStampPosixct, Avg..setting.temp., Indoor.unit.operation.time.accumulation, Suction.air.temp., UnitType, SystemIdentifier, UniqueIdentifier) %>% 
+    select(TimeStampPosixct, Avg..setting.temp., Indoor.unit.thermostat.ON.time.accumulation, Indoor.unit.operation.time.accumulation, Suction.air.temp., UnitType, SystemIdentifier, UniqueIdentifier) %>% 
     mutate(Suction.air.temp. = replace(Suction.air.temp., Suction.air.temp. == 0, NA)) %>% 
     group_by(UniqueIdentifier) %>% 
     fill(Avg..setting.temp., .direction = "downup") %>% 
     ungroup() %>% 
-    mutate(unit_size = get_unit_size(UnitType), 
-           setpoint_factor = Avg..setting.temp.*unit_size, 
+    mutate(unit_size = get_unit_size(UnitType),
+           setpoint_weight = if_else(Indoor.unit.operation.time.accumulation == 0, 0, unit_size * Indoor.unit.thermostat.ON.time.accumulation/Indoor.unit.operation.time.accumulation),
+           setpoint_factor = Avg..setting.temp.* setpoint_weight, 
            ope_time_factor = Indoor.unit.operation.time.accumulation/60 * unit_size,
-           suction_factor = Suction.air.temp. * unit_size,
-           suction_factor_opetime = Suction.air.temp. * unit_size * Indoor.unit.operation.time.accumulation/60) %>% 
+           suction_factor = Suction.air.temp. * setpoint_weight
+           # suction_factor_opetime = Suction.air.temp. * unit_size * Indoor.unit.operation.time.accumulation/60
+           )
+  
+  indoors_average_values <- indoors_average_values %>% 
     group_by(TimeStampPosixct) %>% 
-    summarise(setpoint_factor_sum = sum(setpoint_factor, na.rm = TRUE), 
-              unit_size_sum = sum(unit_size, na.rm = TRUE), 
-              average_setpoint=setpoint_factor_sum/unit_size_sum,
+    summarise(unit_size_sum = sum(unit_size, na.rm = TRUE),
+              setpoint_factor_sum = sum(setpoint_factor, na.rm = TRUE), 
+              setpoint_weight_sum = sum(setpoint_weight, na.rm = TRUE), 
+              average_setpoint=setpoint_factor_sum/setpoint_weight_sum,
               ope_time_factor_sum = sum(ope_time_factor, na.rm = TRUE), 
               average_ope_time = ope_time_factor_sum/unit_size_sum, 
               suction_factor_sum = sum(suction_factor, na.rm = TRUE),
-              suction_factor_opetime_sum = sum(suction_factor_opetime, na.rm = TRUE),
-              average_suction = suction_factor_sum/unit_size_sum,
-              average_suction_opetime = if_else(ope_time_factor_sum == 0, average_suction ,suction_factor_opetime_sum/ope_time_factor_sum)) %>% 
+              # suction_factor_opetime_sum = sum(suction_factor_opetime, na.rm = TRUE),
+              average_suction = suction_factor_sum/setpoint_weight_sum
+              # average_suction_opetime = if_else(ope_time_factor_sum == 0, average_suction ,suction_factor_opetime_sum/ope_time_factor_sum)
+              ) %>% 
     ungroup() %>% 
-    mutate(average_suction = replace_na(average_suction, 0), dummy.worktime = if_else(average_ope_time == 0, 0, 1))%>% 
+    mutate(average_suction = replace_na(average_suction, 0),
+           # dummy.worktime = if_else(average_ope_time == 0, 0, 1),
+           avg_suction_lag_diff = average_suction - lag(average_suction)) %>%
     select(-contains("sum"))
   
   total_inc_tec_tsbl <- outdoor_inc_tec_df %>% 
@@ -58,7 +66,11 @@ prepare_df_fun <- function(Data_struct_fun) map(Data_struct_fun$Out_DF_list_outd
     as_tsibble(index = TimeStampPosixct, key = UniqueIdentifier) %>% 
     fill_gaps() %>% 
     fill(everything(), .direction = "downup") %>% 
-    mutate(hd = hour(TimeStampPosixct), my = month(TimeStampPosixct), wd = wday(TimeStampPosixct, week_start = getOption("lubridate.week.start", 1)), OpeMode = factor(OpeMode, levels = c("Cooling", "Heating", "HeRec", "Both", "Idle"))) %>% 
+    mutate(hd = hour(TimeStampPosixct),
+           my = month(TimeStampPosixct),
+           wd = wday(TimeStampPosixct, week_start = getOption("lubridate.week.start", 1)),
+           tow = hour(TimeStampPosixct) + lubridate::minute(TimeStampPosixct)/60 + wday(TimeStampPosixct, week_start = getOption("lubridate.week.start", 1)) * 24,
+           OpeMode = factor(OpeMode, levels = c("Cooling", "Heating", "HeRec", "Both", "Idle"))) %>% 
     mutate(dummy.OpeMode = OpeMode)
   
   # add dummmy variables for OpeMode ---------
@@ -85,8 +97,13 @@ possibly_prepare_df_fun <- possibly(prepare_df_fun, otherwise = NULL)
 corr_coef <- function(df) as_tibble(df) %>% 
   select_if(is.numeric) %>% 
   corrr::correlate() %>% 
-  filter(rowname == "consumption") %>% 
+  filter(term == "consumption") %>% 
   select_if(~any(!is.na(.)))
+
+corr_network <- function(df) as_tibble(df) %>% 
+  select_if(is.numeric) %>% 
+  corrr::correlate() %>%
+  corrr::network_plot(min_cor = .2)
 
 # add CV to the model accuracy  -------
 accuracy_cv_fun <- function(outdoor_df, out_model) mutate(accuracy(out_model), mean_cons = mean(outdoor_df$consumption), CV = RMSE/mean_cons*100)
@@ -163,16 +180,23 @@ add_non_working_time <- function(df) {
 
 apply_eco_shut_down_cooling <- function(df){
   if (!("non_working_time" %in% names(df))) df <- add_non_working_time(df)
-  setback_df <- mutate(df, dummy.worktime_out = if_else(OpeMode == "Cooling" & non_working_time, 0, dummy.worktime_out))
+  setback_df <- mutate(df,
+                       dummy.worktime_out = if_else(OpeMode == "Cooling" & non_working_time, 0, dummy.worktime_out),
+                       ope_time_out = if_else(OpeMode == "Cooling" & non_working_time, 0, ope_time_out),
+                       average_ope_time = if_else(OpeMode == "Cooling" & non_working_time, 0, average_ope_time)
+                       )
   return(setback_df)
 } 
 
-apply_eco_non_work_time_setback_heating <- function(df, Tstbk_heat = 18) {
+apply_eco_non_work_time_setback_heating <- function(df, Tstbk_heat = 18, continuous_run = TRUE) {
   if (!("non_working_time" %in% names(df))) df <- add_non_working_time(df)
   setback_df <- mutate(df,
     average_setpoint = if_else(OpeMode == "Heating" & non_working_time & average_setpoint > Tstbk_heat, Tstbk_heat, average_setpoint),
     average_suction = if_else(OpeMode == "Heating" & non_working_time & average_suction > Tstbk_heat, Tstbk_heat, average_suction)
   )
+  setback_df <- mutate(setback_df, avg_suction_lag_diff = average_suction - lag(average_suction))
+  if(continuous_run) setback_df <- mutate(setback_df, dummy.worktime = 1,
+                                          average_ope_time = 1)
   return(setback_df)
 }
 
@@ -187,6 +211,7 @@ apply_eco_shift_setpoint <- function(df, setback = 2, Tsopt_cool = 26, Tsopt_hea
     OpeMode == "Heating" ~ setback_fun_vec(cut_dir = "down", temperature_var = average_suction, setback = setback, Tsopt = Tsopt_heat),
     TRUE ~ average_suction
   ))
+  setback_df <- mutate(setback_df, avg_suction_lag_diff = average_suction - lag(average_suction))
   return(setback_df)
 }
 
@@ -207,7 +232,7 @@ apply_eco_tc_shift <- function(df, Tc_setback = 2, Tc_limit = 41) {
 }
 
 # function to create dataframe with setback
-setback_fun <- function(df, suction_stbck = 2, Te_setback = 5, Tc_setback = 5, ambient_shift = 0) {
+setback_fun <- function(df, suction_stbck = 2, Te_setback = 0, Tc_setback = 0, ambient_shift = 0) {
   setback_df <- df
   # setback_df <- add_calendar_fields_fun(df)
   # setback_df <- mutate(setback_df, non_working_time = case_when(
@@ -229,7 +254,7 @@ setback_fun <- function(df, suction_stbck = 2, Te_setback = 5, Tc_setback = 5, a
     OpeMode == "Heating" ~ setback_fun_vec(cut_dir = "down", temperature_var = average_suction, setback = suction_stbck, Tsopt = -Inf),
     TRUE ~ average_suction
   ))
-  
+  setback_df <- mutate(setback_df, avg_suction_lag_diff = average_suction - lag(average_suction))
     # average_suction_opetime = case_when(
     #   OpeMode == "Cooling" & is.null(Tsopt_cool) ~ average_suction_opetime + suction_stbck,
     #   OpeMode == "Heating" & is.null(Tsopt_heat) ~ average_suction_opetime - suction_stbck,
@@ -263,19 +288,20 @@ setback_fun <- function(df, suction_stbck = 2, Te_setback = 5, Tc_setback = 5, a
 
 # function to compare real, forecast from training and setback consumption -------
 compare_cons_fun <- function (training_df, forecast_real, forecast_stbck){
+  
   ope_mode <- map_dfr(list(training_df, forecast_real, forecast_stbck), ~distinct(.x, OpeMode)) %>% 
     distinct(OpeMode) %>% 
     pull(OpeMode)
   
   real_cons_sum <- sum(training_df$consumption)
   # browser()
-  comparison_df <- as_tibble(forecast_real) %>% 
+  comparison_df <- as_tibble(forecast_real) %>%
     group_by(.model) %>% 
-    summarise(fcst_sum = sum(consumption, na.rm = T)) %>% 
+    summarise(fcst_sum = sum(.mean, na.rm = T)) %>% 
     full_join(forecast_stbck %>% 
                 as_tibble() %>% 
                 group_by(.model) %>% 
-                summarise(fcst_setback_sum = sum(consumption, na.rm = T)) ,
+                summarise(fcst_setback_sum = sum(.mean, na.rm = T)) ,
               by = ".model") %>% 
     mutate(OpeMode = ope_mode, real_cons = real_cons_sum, saving_kwh = fcst_sum - fcst_setback_sum, saving_perc = saving_kwh/fcst_sum*100)
   return(comparison_df)
@@ -292,8 +318,9 @@ rename_mode <- function(orig_name, ope_mode) {
 add_mode_columns <- function (df, ope_mode, model_formula){
   # browser()
   orig_names <- names(df)
-  df <- mutate(df, data = map(hourly_df, ~filter(.x, OpeMode == ope_mode & dummy.worktime == 1)))
+  df <- mutate(df, data = map(hourly_df, ~filter(.x, OpeMode == ope_mode)))
   df <- mutate(df, corr_coef = map(data, possibly(~corr_coef(.x), otherwise = NA_real_)))
+  df <- mutate(df, corr_network = map(data, possibly(~corr_network(.x), otherwise = NA_real_)))
   df <- mutate(df, out_model = map(data, possibly(~model(.x, TSLM = TSLM(model_formula)), otherwise = NA_real_)))
   df <- mutate(df, regr_coef = map(out_model, possibly(~coef(.x), otherwise = NA_real_)))
   df <- mutate(df, n_rows = map_dbl(data, ~nrow(.x)))
@@ -375,6 +402,9 @@ grid_plt <- cowplot::plot_grid(plotlist = samples_df_lst)
 return (grid_plt)
 }
 
+facet_eco_lbl <- function(lbl) case_when(lbl == "forecast_real" ~ "without ECO", 
+                                         lbl == "forecast_stbck" ~ "with ECO")
+
 # move this into main markdown ------
 weekly_plt_comparison_fun <- function (forecast_real, forecast_stbck, consumption_var) {
   consumption_var <- enquo(consumption_var)
@@ -382,21 +412,22 @@ weekly_plt_comparison_fun <- function (forecast_real, forecast_stbck, consumptio
   df_lst <- list(forecast_real, forecast_stbck) %>% 
     set_names(c("forecast_real", "forecast_stbck"))
   
-  df <- imap(df_lst, function (df, df_name) {
+  df <- imap_dfr(df_lst, function (df, df_name) {
     if (!("Week_Day_Name" %in% names(df))) df <- add_calendar_fields_fun(df)
     if (!("Week_day_consumption" %in% names(df))) df <- as_tibble(df) %>% 
         group_by(Week_Day, Week_Day_Name, OpeMode, workday) %>%
         summarise(Week_day_consumption = sum(!!consumption_var, na.rm = TRUE)) %>% 
         ungroup() %>% 
+        expand_week_df_fun() %>% 
         mutate(df_name = df_name) %>% 
-        mutate(Week_day_consumption = if_else(Week_day_consumption < 0, 0, Week_day_consumption))
+        mutate(Week_day_consumption = if_else(Week_day_consumption < 0, 0, Week_day_consumption)) %>% 
+        filter(!(Week_Day == 0 & is.na(Week_Day_Name)))
     return(df)
-  }) %>% 
-    bind_rows()
+  })
   # main plt
  plt <- weekly_plt_fun(df)
  plt <- plt +
-    facet_wrap(~ df_name, ncol = 2)
+    facet_wrap(~ df_name, ncol = 2, labeller = as_labeller(facet_eco_lbl))
   
   return(plt)
 }
@@ -407,7 +438,7 @@ daily_plt_comparison_fun <- function (forecast_real, forecast_stbck, consumption
   df_lst <- list(forecast_real, forecast_stbck) %>% 
     set_names(c("forecast_real", "forecast_stbck"))
   
-  df <- imap(df_lst, function (df, df_name) {
+  df <- imap_dfr(df_lst, function (df, df_name) {
     if (!("Week_Day_Name" %in% names(df))) df <- add_calendar_fields_fun(df)
     if (!("Week_day_consumption" %in% names(df))) df <- as_tibble(df) %>% 
         group_by(Day_Hour, OpeMode, Night_Time) %>%
@@ -417,12 +448,12 @@ daily_plt_comparison_fun <- function (forecast_real, forecast_stbck, consumption
         mutate(Day_hour_consumption = if_else(Day_hour_consumption < 0, 0, Day_hour_consumption))
     return(df)
   }) %>% 
-    bind_rows()
+    filter(!is.na(Day_Hour)) 
   
   # main plt
   plt <- daily_plt_fun(df)
   plt <- plt +
-    facet_wrap(~ df_name, ncol = 2)
+    facet_wrap(~ df_name, ncol = 2, labeller = as_labeller(facet_eco_lbl))
   
   return(plt)
 }
@@ -438,19 +469,28 @@ setpoint_plt_comparison_fun <- function (forecast_real, forecast_stbck, consumpt
                select(OpeMode, !!consumption_var, !!histogram_var) %>% 
                mutate(df_name = df_name)) %>% 
     bind_rows()
-  ope_mode <- pull(distinct(df, OpeMode), OpeMode)
+  ope_mode <- pull(filter(distinct(df, OpeMode), !is.na(OpeMode)), OpeMode)
   
   # main plt
   plt <- ggplot(data = df, aes(x = !!histogram_var, weights = !!consumption_var))+
     geom_histogram(fill = Color_Mode_for_plot[ope_mode]) +
     labs(title=str_c(ope_mode , ""))+
     My_ggplot_opts+
-    facet_wrap(~ df_name, ncol = 2)
+    facet_wrap(~ df_name, ncol = 2, labeller = as_labeller(facet_eco_lbl))
   
   return(plt)
 }
 
-
+flextable_pretty <- function(tbl){
+  format_fun <- function (x) if_else(abs(x) < 1, sprintf("%#.3g", x), prettyNum(x, digits = 3))
+  num_names <-  names(select(as_tibble(tbl$body$dataset), where(is.numeric)))
+  format_lst <- setNames(rep(list(format_fun), length(num_names)), num_names)
+  tbl <- border_remove(tbl) %>% 
+    set_formatter(values = format_lst) %>% 
+    border(border.bottom = fp_border(color = daikin_cols("dark grey"), width = 1), part = "header") %>%
+    border(border.bottom = fp_border(color = daikin_cols("grey 60"), width = 0.3), part = "body")
+  return(tbl)
+}
 
 
 
